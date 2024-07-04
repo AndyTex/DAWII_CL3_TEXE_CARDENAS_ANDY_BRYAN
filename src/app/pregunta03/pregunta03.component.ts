f218eecaf597962daafd1e2859228fea4ea4e5f2
WhiteNight07181 <andycnblue1234@gmail.com>
2024-07-04 04:55:05 -0500

    tercera pregunta 2 commit

--- src/app/pregunta03/pregunta03.component.ts
+++ src/app/pregunta03/pregunta03.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit,ViewChild } from '@angular/core';
+import { Todo } from './todos';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatTableModule } from '@angular/material/table';
+import { TodosService } from '../todos.service';
+import { NgClass } from '@angular/common';
+import { MaterialModule } from '../angular-material/material/material.module';
 
 @Component({
   selector: 'app-pregunta03',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule,MatTableModule,MatPaginatorModule,NgClass],
   templateUrl: './pregunta03.component.html',
   styleUrl: './pregunta03.component.css'
 })
-export class Pregunta03Component {
+export class Pregunta03Component implements OnInit {
+  displayedColumns: string[] = ['id', 'title', 'completed'];
+  dataSource = new MatTableDataSource<Todo>();
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  constructor(private todoService: TodosService) {}
+
+  ngOnInit(): void {
+    this.todoService.getFilteredTodos().subscribe(data => {
+      this.dataSource.data = data;
+      this.dataSource.paginator = this.paginator;
+    });
+  }
 
 }
--- src/app/todos.service.ts
+++ src/app/todos.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Todo } from './pregunta03/todos';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodosService {
 
-  constructor() { }
+  private apiUrl = 'https://jsonplaceholder.typicode.com/todos';
+
+  constructor(private http: HttpClient) { }
+
+  getFilteredTodos(): Observable<Todo[]> {
+    return this.http.get<Todo[]>(this.apiUrl).pipe(
+      map(todos => todos.filter(todo => todo.id >= 25 && todo.id <= 90 && todo.completed))
+    );
+  }
 }
