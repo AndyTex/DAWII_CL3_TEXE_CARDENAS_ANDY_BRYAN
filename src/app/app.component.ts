14313989f0919a53c832ead3f062e6a6cab24198
WhiteNight07181 <andycnblue1234@gmail.com>
2024-07-04 02:36:27 -0500

    primera pregunta 2 commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
+import { MaterialModule } from './angular-material/material/material.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MaterialModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'DAWII_CL3_TEXE_CARDENAS_ANDY_BRYAN';
+
+  constructor(private router:Router, private activatedRoute: ActivatedRoute){}
+
+  Pregunta2() {
+    this.router.navigate(['/pregunta02']);
+  }
+
+  Pregunta3() {
+    this.router.navigate(['/pregunta03']);
+  }
+
+  Bienvenido() {
+    this.router.navigate(['/']);
+  }
+
 }
